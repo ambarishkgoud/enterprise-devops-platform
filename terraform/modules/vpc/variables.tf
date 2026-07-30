726be0cb9d8f53b3987b140fbb922cf907de20f4
ambarishkgoud <ambarishkgoud@gmail.com>
2026-07-30 13:50:58 -0500

    Added main.tf file and formatted the code

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,45 +1,45 @@
 variable "vpc_name" {
-    description = "Name of the VPC"
-    type = string
-  
+  description = "Name of the VPC"
+  type        = string
+
 }
 
 variable "vpc_cidr" {
-    description = "CIDR block for the VPC"
-    type = string
+  description = "CIDR block for the VPC"
+  type        = string
 
-    validation {
-      condition = can(cidrnetmask(var.vpc_cidr))
-      error_message = "Please provide a valid CIDR block."
-    }
-  
+  validation {
+    condition     = can(cidrnetmask(var.vpc_cidr))
+    error_message = "Please provide a valid CIDR block."
+  }
+
 }
 
 variable "enable_dns_support" {
-    description = "Enable DNS Support"
-    type = bool
-    default = true
-  
+  description = "Enable DNS Support"
+  type        = bool
+  default     = true
+
 }
 
 variable "enable_dns_hostnames" {
-    description = "Enable DNS Hostnames"
-    type = bool
-    default = true
-  
+  description = "Enable DNS Hostnames"
+  type        = bool
+  default     = true
+
 }
 
 variable "environment" {
-    description = "Deployment environment"
-    type = string
+  description = "Deployment environment"
+  type        = string
 
-    validation {
-      condition = contains (
-        ["dev", "qa", "stage", "prod"],
-        var.environment
-      )
+  validation {
+    condition = contains(
+      ["dev", "qa", "stage", "prod"],
+      var.environment
+    )
 
-      error_message = "Environment must be dev, qa, stage, or prod."
-    }
-  
+    error_message = "Environment must be dev, qa, stage, or prod."
+  }
+
 }
